9642b1452fd8608a6a864ff0822a936b60c040f9
Marcus Cevolela <marcus.cevolela@gmail.com>
2026-04-07 19:19:26 -0300

    Reorganização da estrutura base do projeto (assets, scripts, styles)

--- script.js
+++
@@ -1,4 +0,0 @@
-function toggleMenu() {
-    document.getElementById('menu').classList.toggle('aberto');
-    document.getElementById('overlay').classList.toggle('ativo');
-}
---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    document.getElementById('menu').classList.toggle('aberto');
+    document.getElementById('overlay').classList.toggle('ativo');
+}
